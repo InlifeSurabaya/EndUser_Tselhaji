bfd391d3a0a1a3095488b601958200caa0920518
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-10-18 00:08:57 +0700

    feat(Product): add SoftDeletes trait and define country relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'detail', 'harga', 'quota_amount', 'quota_type', 'validity_days', 'discount', 'is_active', 'country_id'];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(CategoryCountryProduct::class);
+    }
 }
