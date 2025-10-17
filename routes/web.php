6bb61875ab9046e2e022ad8553636a8675269188
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-10-17 22:22:02 +0700

    feat(Dashboard): create Dashboard component and set up routing

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,15 @@ use App\Livewire\Auth\Register;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Auth\Login;
 use App\Livewire\Product\IndexProduct;
+use App\Livewire\Dashboard;
+
 
 // === AUTH ROUTE ===
-Route::get('/', Login::class)->name('login');
+Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('register');
 
-// === INDEX ROUTE ===
-
+// === DASHBOARD ROUTE ===
+Route::get('/', Dashboard::class)->name('dashboard');
 
 // === PRODUCT ROUTE ===
 Route::get('/product', IndexProduct::class)->name('index.product');
