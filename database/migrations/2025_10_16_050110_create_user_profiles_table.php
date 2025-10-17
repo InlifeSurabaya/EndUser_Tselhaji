9958e62811dc626583162737f5a21d32edadc1b3
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-10-18 00:09:08 +0700

    feat(Migration): add index to the ID column in user_profiles table

--- database/migrations/2025_10_16_050110_create_user_profiles_table.php
+++ database/migrations/2025_10_16_050110_create_user_profiles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_profiles', function (Blueprint $table) {
-            $table->id();
+            $table->id()->index();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('fullname')->nullable();
             $table->string('gender')->nullable();
