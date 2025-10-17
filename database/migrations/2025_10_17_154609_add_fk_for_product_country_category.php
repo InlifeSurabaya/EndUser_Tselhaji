7f34fac9972b0e79e8628e1a16ad03e21be00b5e
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-10-18 00:09:20 +0700

    feat(Migration): add foreign key constraint for country_id in products table

---
+++ database/migrations/2025_10_17_154609_add_fk_for_product_country_category.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            if (!Schema::hasColumn('products', 'country_id')) {
+                $table->unsignedBigInteger('country_id')->after('id')->index();
+
+                $table->foreign('country_id')
+                    ->references('id')
+                    ->on('category_country_products')
+                    ->cascadeOnDelete();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['country_id']);
+            $table->dropColumn('country_id');
+        });
+
+    }
+};
