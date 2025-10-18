7643ea8b89f8b91c62c7ac14b99419142f939ae5
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-10-18 11:02:11 +0700

    fix(app.blade.php): correct title default and improve HTML structure

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
+    <title>{{ $title ?? 'Tsel Haji' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="h-full bg-neutral-50 font-sans antialiased">
+    <div class="flex flex-col min-h-screen">
         <livewire:components.navbar/>
 
-        {{ $slot }}
+        <main class="flex-grow">
+            {{ $slot }}
+        </main>
 
         <livewire:components.footer/>
-    </body>
+    </div>
+</body>
 </html>
