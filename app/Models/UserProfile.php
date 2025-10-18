ffc68a0a0f21ea901c0dd162c1c9feb354947db7
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-10-18 11:01:40 +0700

    feat(UserProfile): create Livewire component and view for user profile

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserProfile extends Model
@@ -12,4 +13,9 @@ class UserProfile extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['user_id', 'fullname', 'gender', 'birth_date', 'phone', 'address', 'avatar'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
