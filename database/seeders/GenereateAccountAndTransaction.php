035916295a0864ffcbfa93f363ff80fa286e794b
raihan <r1aihan2003f1irdaus@gmail.com>
2026-01-13 08:32:10 +0700

    add seeder

--- database/seeders/GenereateAccountAndTransaction.php
+++ database/seeders/GenereateAccountAndTransaction.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Livewire\User\UserProfile;
 use App\Models\Order;
 use App\Models\Transaction;
 use App\Models\User;
+use App\Models\UserProfile;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
